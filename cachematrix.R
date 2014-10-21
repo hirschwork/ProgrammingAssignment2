ca014c9dcbf6dc4bca94dbce1d634c8f2a3ad608
hirsch malik <hirschwork@gmail.com>
2014-10-21 22:03:24 +0200

    Filled in missing code for functions makeCacheMatrix() and cacheSolve()

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,25 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  get <- function() x
+  getinverse <- function () solve(x)
+  set <- function(y) {
+    CM <<- NULL
+    x <<- y
+  }
+  list(get=get, getinverse= getinverse, set=set)
 }
-
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  if (!is.null(CM)) {
+    print ("getting from cache..")
+    CM }
+  else {
+      z <- solve(x$get())
+      CM <<- z
+      CM
+  }
 }
